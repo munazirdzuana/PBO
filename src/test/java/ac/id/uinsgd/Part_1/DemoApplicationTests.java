283d21b5a3e3feedd4693e0e02694974352423db
munazirdzuana <78277922+munazirdzuana@users.noreply.github.com>
2022-03-09 20:37:49 +0700

    Add files via upload

---
+++ src/test/java/ac/id/uinsgd/Part_1/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.id.uinsgd.Part_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
